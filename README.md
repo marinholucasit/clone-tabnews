ea606ad4df2db78db7520cc3cc021148b33782a5
Lucas Marinho Gomes <56567814+marinholucasit@users.noreply.github.com>
2023-09-21 11:56:51 +0000

    add prettier and configuration

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 A project to improve my programming skills
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Curtiu nossa viagem? Então dê uma rizadinha! :D  </h1>
+  return <h1>Curtiu nossa viagem? Então dê uma rizadinha! :D </h1>;
 }
 
 export default Home;

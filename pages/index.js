b5ad65c77ea2677211c6a1d27bbd6c5d935117da
Lucas Marinho Gomes <56567814+marinholucasit@users.noreply.github.com>
2023-09-27 18:07:50 +0000

    modificando a msg

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
 function Home() {
-  return <h1>Curtiu nossa viagem? Então dê uma rizadinha! :D </h1>;
+  return (
+    <h1>
+      Fala galera, esse é um projeto pessoal que estou iniciando e no momento
+      somente as pessoas mais proximas sabem desse domínio
+      leigoinvestidor.com.br Também estou criando um canal no youtube (mas ainda
+      está sem vídeos hehe) Por enquanto gostaria que você seguisse o perfil no
+      Insta: @umleigo.invest pra dar uma força :D Obrigado!
+    </h1>
+  );
 }
 
 export default Home;

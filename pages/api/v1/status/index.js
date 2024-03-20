1e23486a49108671f338c68cfada9dc9f40e71b5
Lucas Marinho Gomes <56567814+marinholucasit@users.noreply.github.com>
2024-03-20 11:13:03 +0000

    add max and open connections

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,37 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as SUM");
+  const updatedAt = new Date().toISOString();
 
-  response.status(200).json({ chave: "Deu certo" });
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersion = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnection =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionResult = await database.query(
+    {
+      text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+      values: [databaseName]
+    }
+  );
+
+  const databaseOpenedConnection = databaseOpenedConnectionResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: parseInt(databaseMaxConnection),
+        open_connections: databaseOpenedConnection,
+      },
+    },
+  });
 }
 
 export default status;

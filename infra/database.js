46d93b9d7ff66b1041a901b44497fd61711943cb
Lucas Marinho Gomes <56567814+marinholucasit@users.noreply.github.com>
2024-05-21 15:05:25 +0000

    fix return from getSSLValues

--- infra/database.js
+++ infra/database.js
@@ -23,7 +23,7 @@ async function query(queryObject) {
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
     return {
-      CA: process.env.POSTGRES_CA,
+      ca: process.env.POSTGRES_CA,
     };
   }
   return process.env.NODE_ENV === "development" ? false : true;
